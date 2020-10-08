8c34daa195240b6602d8f815823f25acbf149926
Nick Sturch-Flint <nick.rulez@live.com>
2020-10-08 13:59:18 -0400

    Created function for dynaic vector to create game objects, and compare 2 game objects

--- OOP3200-F2020-Lesson3c/main.cpp
+++ OOP3200-F2020-Lesson3c/main.cpp
@@ -1,45 +1,75 @@
 #include <iomanip>
 #include <iostream>
+#include <vector>
+
 
 
 #include "GameObject.h"
 
-int main()
+static void BuildGameObjects(std::vector<GameObject*>& game_objects, const int num = 2)
 {
-	int id1, id2;
-	Vector2D point1;
-    std::cout << "Enter the first point (x, y): ";
-	std::cin >> point1;
-	std::cout << "Enter first Game Object ID: ";
-	std::cin >> id1;
-	std::cout << "\n--------------------------------------------------------------" << std::endl;
-	std::cout << "You Entered " << point1 << " for the first Game Object" << std::endl;
-	std::cout << "--------------------------------------------------------------\n" << std::endl;
-	const GameObject gameObject1(id1, point1);
 
-	Vector2D point2;
-	std::cout << "Enter the second point (x, y): ";
-	std::cin >> point2;
-	std::cout << "Enter second Game Object ID: ";
-	std::cin >> id2;
-	std::cout << "\n--------------------------------------------------------------" << std::endl;
-	std::cout << "You Entered " << point2 << " for the second Game Object" << std::endl;
-	std::cout << "--------------------------------------------------------------\n" << std::endl;
-	const GameObject gameObject2(id2, point2);
+	for (auto count = 0; count < num; count++)
+	{
+		int id;
+		std::cout << "Enter the Game Object ID: ";
+		std::cin >> id;
 
+		Vector2D point;
+		std::cout << "Enter the Game Object's points (x, y): ";
+		std::cin >> point;
+
+		std::cout << "\n--------------------------------------------------------------" << std::endl;
+		std::cout << "You Entered " << id << " for the Game Object ID" << std::endl;
+		std::cout << "You Entered " << point << " for the Game Objects position" << std::endl;
+		std::cout << "--------------------------------------------------------------\n" << std::endl;
+		auto* gameObject = new GameObject(id, point);
+		game_objects.push_back(gameObject);
+	}
+
+
+
+}
+
+static void CompareGameObjects(GameObject* object1, GameObject* object2)
+{
 	std::cout << std::fixed << std::setprecision(3);
-	std::cout << "Magnitude of first Game Object is: " << gameObject1.GetPosition().GetMagnitude() << std::endl;
-	std::cout << "Magnitude of second Game Object is: " << gameObject2.GetPosition().GetMagnitude() << std::endl;
-	std::cout << "Distance between first point and second point is: " << Vector2D::Distance(gameObject1.GetPosition(), gameObject2.GetPosition()) << std::endl;
+	std::cout << "Magnitude of first Game Object is: " << object1->GetPosition().GetMagnitude() << std::endl;
+	std::cout << "Magnitude of second Game Object is: " << object2->GetPosition().GetMagnitude() << std::endl;
+	std::cout << "Distance between first point and second point is: " << Vector2D::Distance(object1->GetPosition(), object2->GetPosition()) << std::endl;
 	std::cout << "--------------------------------------------------------------\n" << std::endl;
 
-	std::cout << "The angle (in degrees) from the first vector to the second vector is: " << Vector2D::SignedAngle(gameObject1.GetPosition(), gameObject2.GetPosition()) << std::endl << std::endl;
+	std::cout << "The angle (in degrees) from the first vector to the second vector is: " << Vector2D::SignedAngle(object1->GetPosition(), object2->GetPosition()) << std::endl << std::endl;
 
 
 	std::cout << "First Game Object Details: " << std::endl;
-	std::cout << gameObject1.ToString() << std::endl;
+	std::cout << object1->ToString() << std::endl;
 
 	std::cout << "Second Game Object Details: " << std::endl;
-	std::cout << gameObject2.ToString() << std::endl;
+	std::cout << object2->ToString() << std::endl;
+}
+
+int main()
+{
+
+	std::vector<GameObject*> gameObjects;
+	int numGames;
+	std::cout << "How many Games do you want to play?: ";
+	std::cin >> numGames;
+	std::cout << "\n--------------------------------------------------------------" << std::endl;
+
+
+	BuildGameObjects(gameObjects, numGames);
+
+
+	int index1, index2;
+	std::cout << "What is the first Object Index?: ";
+	std::cin >> index1;
+	std::cout << "\n--------------------------------------------------------------" << std::endl;
+
+	std::cout << "What is the second Object Index?: ";
+	std::cin >> index2;
+	CompareGameObjects(gameObjects[index1], gameObjects[index2]);
+
 }
 
